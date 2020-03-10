a0c69dd66c1446dd9b9cbd2a163cfd0f008b96fb
lscavalcante <18038257>
2020-03-10 20:39:45 -0300

    ajuste no card colocando imagem e cores vindo direto da api

--- lib/pages/home_page/home_controller.dart
+++ lib/pages/home_page/home_controller.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:pokemon/consts/consts_api.dart';
 import 'package:pokemon/models/pokemon.dart';
 import 'package:pokemon/services/pokemon_service.dart';
 part 'home_controller.g.dart';
@@ -6,7 +9,6 @@ part 'home_controller.g.dart';
 class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
-  
   @observable
   List<Pokemon> _pokemons;
 
@@ -18,19 +20,18 @@ abstract class _HomeControllerBase with Store {
 
   @computed
   List<Pokemon> get pokemons {
-   if(_pokemons == null) {
-     return _pokemons = [];
-   }
+    if (_pokemons == null) {
+      return _pokemons = [];
+    }
     return _pokemons;
   }
 
   @action
   setpokemon(value) => _pokemon = value;
- 
 
   @action
-  getPokemon()  async {
-   _pokemons = await PokemonService.getAllPokemon();
-   print(_pokemons.length);
+  getPokemon() async {
+    _pokemons = await PokemonService.getAllPokemon();
   }
+
 }
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:pokemon/consts/consts_api.dart';
 import 'package:pokemon/consts/consts_images.dart';
 import 'package:pokemon/models/pokemon.dart';
 import 'package:pokemon/pages/home_page/home_controller.dart';
@@ -86,21 +88,30 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.only(right: 7, left: 7, top: 7, bottom: 7),
       child: Container(
           decoration: BoxDecoration(
-              color: Colors.green, borderRadius: BorderRadius.circular(20)),
+              color: ConstsApi.getColorType(type: pokemon.type[0]), borderRadius: BorderRadius.circular(20)),
           child: Stack(
             children: <Widget>[
               Positioned(
-                right: 10,
-                bottom: 10,
+                right: 0,
+                bottom: 0,
                 child: Opacity(
                     opacity: 0.1,
                     child: Image.asset(
                       ConstsImages.imagePokeball,
                       color: Colors.white,
-                      height: 100,
-                      width: 100,
+                      height: 130,
+                      width: 130,
                     )),
-              )
+              ),
+              Positioned(
+                  right: 10,
+                  bottom: 10,
+                  child: CachedNetworkImage(
+                    imageUrl: "${pokemon.img}",
+                    placeholder: (context, url) => new Container(
+                      color: Colors.transparent,
+                    ),
+                  )),
             ],
           )),
     );
